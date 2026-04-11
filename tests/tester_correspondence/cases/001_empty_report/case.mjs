3029dd39697f58b1689612e2b7cbd02a94386076
Marek Gaspierik <xgaspim00@vutbr.cz>
2026-04-11 14:59:57 +0200

    chore: deleting unnecessary files

--- tests/tester_correspondence/cases/001_empty_report/case.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import { TestReport } from "../../../../typescript/tester/dist/models.js";
-
-const report = new TestReport({ discovered_test_cases: [], unexecuted: {}, results: {} });
-console.log(JSON.stringify(report, null, 2));
--- typescript/int/prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  printWidth: 99,
-  tabWidth: 2,
-  useTabs: false,
-  singleQuote: false,
-  trailingComma: "es5",
-  semi: true
-};
